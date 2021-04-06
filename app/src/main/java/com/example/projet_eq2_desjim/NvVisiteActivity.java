661f6e493b0b19de020527407b28f9f85b1fa449
jjimenez14 <jjimenez@la-joliverie.com>
2021-04-06 14:52:46 +0200

    ameliorations layout

--- app/src/main/java/com/example/projet_eq2_desjim/NvVisiteActivity.java
+++ app/src/main/java/com/example/projet_eq2_desjim/NvVisiteActivity.java
@@ -1,6 +1,9 @@
 package com.example.projet_eq2_desjim;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,6 +12,22 @@ public class NvVisiteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_nvlvisite);
+
+        Button btnSuivant = (Button) findViewById(R.id.button4);
+        View.OnClickListener ecouteur = new View.OnClickListener(){
+            public void onClick(View v){
+                switch(v.getId()) {
+                    case R.id.button4:
+                        startActivity(new Intent(NvVisiteActivity.this, NvVisiteActivity2.class));
+                        break;
+                }
+            }
+        };
+
+        btnSuivant.setOnClickListener(ecouteur);
+
+
     }
+
 
 }
---
+++ app/src/main/java/com/example/projet_eq2_desjim/NvVisiteActivity2.java
@@ -0,0 +1,13 @@
+package com.example.projet_eq2_desjim;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class NvVisiteActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_nvlvisite2);
+    }
+}
